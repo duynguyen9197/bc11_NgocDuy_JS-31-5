5d614ffec36ddb62155dc546aaff33a3a07b50b3
duynguyen9197 <duynguyenngoc9197@gmail.com>
2021-06-02 22:53:18 +0700

    change duration

--- baitap.js
+++ baitap.js
@@ -6,8 +6,8 @@
  * tạo biến donGia
  * tạo biến soTien
  * nếu soLuong nhỏ hơn 50 => soTien = soLuong*donGia
- * nếu soLuong   <=100 thì => soTien = (soLuong*donGia)*92/100
- * nếu soLuong >100 thì soTien =  (soLuong*donGia)*88/100
+ * nếu 50<= soLuong   <=100 thì => soTien = 49*donGia + (soLuong-49)*donGia*0.92
+ * nếu soLuong >100 thì soTien =49*donGia +51*donGia*0.92+(soLuong-100)*donGia*0.88
  * đầu ra: Xuất soTien
  */
 document.getElementById("ThanhToan").onclick=function(event){
@@ -15,13 +15,16 @@ document.getElementById("ThanhToan").onclick=function(event){
     var hangHoa = document.getElementById("TenHangHoa").value;
     var  soLuong =document.getElementById("SoLuong").value;
     var donGia = document.getElementById("DonGia").value;
-    var soTien=0;
-    if(soLuong<50){
+    var soTien="";
+    
+    if(soLuong<50 && soLuong>=1){
         soTien =soLuong*donGia;
-    }else if(soLuong <=100){
-        soTien = (soLuong*donGia)*92/100;
+    }else if(soLuong <=100 &&  50<= soLuong ){
+        soTien =  49*donGia + (soLuong-49)*donGia*0.92;
+    }else if(soLuong >100){
+        soTien = 49*donGia +51*donGia*0.92+(soLuong-100)*donGia*0.88;
     }else{
-        soTien =  (soLuong*donGia)*88/100;
+        soTien="Số Lượng không hợp lệ"
     }
     document.getElementById("Xuat").innerHTML=hangHoa +"("+soLuong+")"+": " +soTien;
 }
